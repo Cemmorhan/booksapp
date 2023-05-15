cdb787ee6bc8e110643a950e70bd17955eaef662
Cemmorhan <jaime.carrasco.jch@gmail.com>
2023-05-15 14:22:49 +0200

    AyudaMiguel

---
+++ components/MyMenu.js
@@ -0,0 +1,44 @@
+
+function MyMenu() {
+    return (
+        <>
+            <div className="menu">
+
+                <div style={{ backgroundColor: "red" }}>
+                    cosa A
+                </div>
+
+                <div className="menu_opciones">
+
+                    <div className="menu_item">
+                        cosa B
+                    </div>
+                    <div className="menu_item">
+                        cosa B
+                    </div>
+                    <div className="menu_item">
+                        cosa B
+                    </div>
+                    <div className="menu_item">
+                        cosa B
+                    </div>
+                </div>
+            </div>
+            <div className="menu_bottom">
+                <div className="menu_item_bottom">
+                A
+                </div>
+                <div className="menu_item_bottom">
+                B
+                </div>
+                <div className="menu_item_bottom">
+                C
+                </div>
+                <div className="menu_item_bottom">
+                D
+                </div>
+            </div>
+        </>
+    )
+}
+export default MyMenu;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import MyMenu from '../components/MyMenu.js'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="mainContainer">
+    <MyMenu />
+    <Component {...pageProps} />
+    </div>
+    )
 }
