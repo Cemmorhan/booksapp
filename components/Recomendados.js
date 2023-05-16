9574b91ee25e895669659e148f72aa5f935233eb
Cemmorhan <jaime.carrasco.jch@gmail.com>
2023-05-16 13:00:52 +0200

    componetizando el index

---
+++ components/Recomendados.js
@@ -0,0 +1,140 @@
+
+
+function Recomendados({titulo}) {
+    console.log(titulo)
+    return (
+        <>
+        <div className="selector_title">
+            <h3>{titulo}</h3>
+          </div>
+        <div className="contenedor_recomendados">
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+            <div className="caratulas">
+            </div>
+
+
+          </div>
+        </>
+    )
+}
+
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:3000/api/recomendados')
+    const titulo = await res.json()
+    return {
+      props: {
+        titulo
+      }
+    }
+}
+
+export default Recomendados;
