e5f7d077da0b87b36e0e350e130414a35c3fa88d
Cemmorhan <jaime.carrasco.jch@gmail.com>
2023-07-17 12:11:18 +0200

    test6

--- pages/sell/index.js
+++ pages/sell/index.js
@@ -1,23 +1,10 @@
 
 import { withPageAuthRequired, useUser } from '@auth0/nextjs-auth0/client';
-import BuscadorApi from "../../components/BuscadorApi"
-import { Divider, Radio, Table } from 'antd';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { message, Button, Modal } from 'antd';/* 
-import { useFileSystemPublicRoutes } from '@/next.config'; */
-import  ObjetoModal  from "../../components/ObjetoModal";
+
 
 
 export default withPageAuthRequired(function sell(props) {
     const { user, error, isLoading } = useUser();
-    const [renderizado, setRenderizado] = useState(false);
-    const [databooks, setDatabooks] = useState([]);
-    const [book, setBook] = useState(undefined);
-    const [booksMyDB, setBooksMyDB] = useState([]);
-    const [booksInTable, setBooksInTable] = useState([]);
-    const [showMoreBooks, setShowMoreBooks] = useState(false);
-    const [modaldata, setModaldata] = useState([]);
 
 
 
