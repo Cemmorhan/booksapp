e0d64d9b9e3136fb12d0cc554da2ab62711e2763
Cemmorhan <jaime.carrasco.jch@gmail.com>
2023-07-17 12:04:11 +0200

    test2

--- pages/sell/index.js
+++ pages/sell/index.js
@@ -2,7 +2,8 @@
 import { withPageAuthRequired, useUser } from '@auth0/nextjs-auth0/client';
 import BuscadorApi from "../../components/BuscadorApi"
 import { Divider, Radio, Table } from 'antd';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { message, Button, Modal } from 'antd';/* 
 import { useFileSystemPublicRoutes } from '@/next.config'; */
 import  ObjetoModal  from "../../components/ObjetoModal";
