94b57dfaa7e8c7c5f26d5882cae0fecc95b00f90
Cemmorhan <jaime.carrasco.jch@gmail.com>
2023-07-17 12:14:08 +0200

    Sell with S mayus

--- pages/sell/index.js
+++ pages/sell/index.js
@@ -1,48 +1,86 @@
 
 import { withPageAuthRequired, useUser } from '@auth0/nextjs-auth0/client';
-import { Tabs } from 'antd';
-import { Descriptions } from 'antd';
-import Ventas from "@/components/Ventas";
-import { useEffect } from 'react';
-import { useState } from 'react';
-import {
-    Button,
-    Form,
-    Input,
-    Modal,
-    message
-} from 'antd';
+import BuscadorApi from "@/components/BuscadorApi"
+import { Divider, Radio, Table } from 'antd';
+import { useEffect, useState } from 'react';
+import { message, Button, Modal } from 'antd';
+import { useFileSystemPublicRoutes } from '@/next.config';
+import  ObjetoModal  from "../../components/ObjetoModal";
 
 
-
-export default withPageAuthRequired(function Perfil(props) {
+export default withPageAuthRequired(function Sell(props) {
     const { user, error, isLoading } = useUser();
     const [renderizado, setRenderizado] = useState(false);
-    const items = [
+    const [databooks, setDatabooks] = useState([]);
+    const [book, setBook] = useState(undefined);
+    const [booksMyDB, setBooksMyDB] = useState([]);
+    const [booksInTable, setBooksInTable] = useState([]);
+    const [showMoreBooks, setShowMoreBooks] = useState(false);
+    const [modaldata, setModaldata] = useState([]);
+
+
+
+    const columns = [
         {
-            key: '1',
-            label: `Perfil`,
-            children:
-                <Descriptions title="User Info">
-                    <Descriptions.Item ><img src={user.picture} alt=""></img></Descriptions.Item>
-                    <Descriptions.Item label="Nombre de usuario">{props.usuarioAdquirido.nickname}</Descriptions.Item>
-                    <Descriptions.Item label="Nombre">{props.usuarioAdquirido.name} {props.usuarioAdquirido.lastname}  </Descriptions.Item>
-                    <Descriptions.Item label="Correo">{props.usuarioAdquirido.email}</Descriptions.Item>
-                    <Descriptions.Item label="Address">{props.usuarioAdquirido.address}  {props.usuarioAdquirido.zip}  {props.usuarioAdquirido.city}  {props.usuarioAdquirido.state}  {props.usuarioAdquirido.country}</Descriptions.Item>
-                </Descriptions>
-            ,
+            title: 'Título',
+            dataIndex: 'title',
+            render: (text) => <a>{text}</a>,
         },
         {
-            key: '2',
-            label: `Historial`,
-            children: `Content of Tab Pane 2`,
+            title: 'Autor',
+            dataIndex: 'author',
         },
         {
-            key: '3',
-            label: `Ventas`,
-            children: <Ventas ></Ventas>,
-        },
+            title: 'Idioma',
+            dataIndex: 'language',
+        }
     ];
+
+    // rowSelection object indicates the need for row selection
+    const rowSeletionTable = {
+
+        book,
+        onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+            setBook(selectedRows[0]);
+        },
+
+    };
+    // Guardar libros mongo
+    const getBook = async () => {
+        console.log("selectedbokinsidegetbook", book)
+        if (!book) return;
+        const send = {
+            book_id: book.id,
+            title: book.title,
+            author: book.author,
+            language: book.language,
+            description: book.description,
+            isbn: book.isbn,
+            year: book.year,
+            pages: book.pages,
+            genre: book.genre,
+            publisher: book.publisher,
+            image: book.image,
+            raw: book.raw,
+            user: user,
+            /*
+            price: book_price,
+            updatedate: book_updatedate,
+            selldate: book_selldate,
+            state: book_state*/
+        };
+
+        console.log("cosas que envío", send)
+        const results = await fetch("/api/createbook", {
+            method: "POST",
+            body: JSON.stringify(send),
+        }).then((response) => response.json());
+        const newResults = results.map((result) => {
+            return { ...result }
+        });
+        setBook(newResults);
+    };
     //metodo post para recuperar datos o crear usuario
     const getUsers = async () => {
         if (!user) return;
@@ -56,29 +94,6 @@ export default withPageAuthRequired(function Perfil(props) {
         });
         props.setUsuarioAdquirido(newResults[0]);
     };
-    //actualizar datos usuario
-    const updateUsers = async (values) => {
-        if (!user) return;
-        const send = {
-            user_id: user.sub,
-            set: values.set === undefined ? {} : values.set,
-            push: values.push === undefined ? {} : values.push,
-            inc: values.inc === undefined ? {} : values.inc,
-        };
-        const results = await fetch("/api/updateuser", {
-            method: "POST",
-            body: JSON.stringify(send),
-        }).then((response) => response.json());
-        const newResults = results.map((result) => {
-            return { ...result }
-        });
-        props.setUsuarioAdquirido(newResults);
-    };
-
-    function setSaldo() {
-        console.log("setSaldo");
-        updateUsers({ inc: { saldo: 1000 } });
-    }
 
     useEffect(() => {
         setRenderizado(true);
@@ -91,30 +106,42 @@ export default withPageAuthRequired(function Perfil(props) {
     }, [renderizado]);
 
     useEffect(() => {
-        console.log(props.usuarioAdquirido);
-    }, [props.usuarioAdquirido]);
+        if (renderizado && showMoreBooks) {
+            const booksShown = [...booksMyDB, ...databooks];
+            const bookskey1 = booksShown.map((result, index) => { return { ...result, key: index } });
+            setBooksInTable(bookskey1);
+        }
+        else if (renderizado && !showMoreBooks) {
+            const bookskey2 = booksMyDB.map((result, index) => { return { ...result, key: index } });
+            setBooksInTable(bookskey2);
+        }
 
+    }, [renderizado, showMoreBooks, databooks, booksMyDB]);
+useEffect(() => {
+    if (renderizado) {  
+        setModaldata(book!=undefined?<ObjetoModal books={[book]}></ObjetoModal>:<h2>No se encuentra el libro</h2>)
+    }
+}, [renderizado,book]);
 
+    // Venta
+    function Venta() {
+        //getBook();
+        showModal();
+    }
+    function showMore() {
+        setShowMoreBooks(true);
+    }
 
-
-
-    // Parte del formulario
-    const onFinish = (values) => {
-        console.log('Success:', values);
-        updateUsers({ set: values });
-        setIsModalOpen(false);
-        successMessage();
-
-
-
+    // Mensajes
+    const [messageApi, contextHolder] = message.useMessage();
+    const success = () => {
+        messageApi.open({
+            type: 'success',
+            content: 'This is a success message',
+        });
     };
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-        errorMessage();
-    };
 
-    //parte modal
-
+    // mensaje modal
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
         setIsModalOpen(true);
@@ -127,36 +154,57 @@ export default withPageAuthRequired(function Perfil(props) {
     };
 
 
-    // messages
-    const [messageApi, contextHolder] = message.useMessage();
-    const successMessage = () => {
-        messageApi.open({
-            type: 'success',
-            content: 'This is a success message',
-        });
-    };
-    const errorMessage = () => {
-        messageApi.open({
-            type: 'error',
-            content: 'This is an error message',
-        });
-    };
-
-
     return (
         <>
             {contextHolder}
             <div className="content">
-                <div className="tabsperfil">
-                    <Tabs defaultActiveKey="1" items={items} size="large" />
-                    <div className='tabsbotton'>
-                        <Button type="primary" onClick={showModal}>
-                            Editar perfil      </Button>
-                    </div>
+                <BuscadorApi setDatabooks={setDatabooks} setBooksMyDB={setBooksMyDB} />
+                <div className="results_books" style={{ padding: "10px 20px", width: "100%" }}>
+
+                    {booksInTable.length > 0 ? <Table
+
+                        style={{ width: "100%" }}
+                        rowSelection={{
+                            type: "radio",
+                            ...rowSeletionTable,
+                        }}
+                        columns={columns}
+                        dataSource={booksInTable}
+                        expandable={{
+                            expandedRowRender: (record) => (<>
+                                <p style={{ margin: 0 }}>Resumen:</p>
+                                <p
+                                    style={{
+                                        margin: 0,
+                                    }}
+                                >
+                                    {record.description}
+                                </p>
+                                <p style={{ margin: 0 }}>ISBN:</p>
+                                <p
+                                    style={{
+                                        margin: 0,
+                                    }}
+                                >
+                                    {record.isbn}
+                                </p>
+                            </>
+
+                            ),
+                            rowExpandable: (record) => record.name !== 'Not Expandable',
+                        }}
+                    /> : null}
+
                 </div>
-                <button onClick={setSaldo}>Subir saldo</button>
-                <br></br>
-                <Modal title="Editar perfil"
+                {databooks.length > 0 ? <div style={{ display: "flex", justifyContent: "right", alignItems: "center", flexDirection: "column" }}>
+                    <h4>
+                        No encuentras el libro? Impórtalo de Google Books y añádelo a tu biblioteca</h4>
+
+                    <Button type="primary" onClick={showMore} style={{ margin: "20px 0px" }}>
+                        Mostrar
+                    </Button>
+                </div> : null}
+                <Modal title="Es este tu libro? Véndelo!"
                     open={isModalOpen}
                     onOk={handleOk}
                     onCancel={handleCancel}
@@ -165,156 +213,18 @@ export default withPageAuthRequired(function Perfil(props) {
 
                     width={800}
                 >
-                    <Form
-
-                        name="basic"
-
-                        labelCol={{ span: 8 }}
-                        wrapperCol={{ span: 16 }}
-                        initialValues=
-                        {
-                            {
-                                name: props.usuarioAdquirido.name,
-                                lastname: props.usuarioAdquirido.lastname,
-                                address: props.usuarioAdquirido.address,
-                                city: props.usuarioAdquirido.city,
-                                country: props.usuarioAdquirido.country,
-                                state: props.usuarioAdquirido.state,
-                                zip: props.usuarioAdquirido.zip,
-
-
-
-                            }
-                        }
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-
-
-                        <Form.Item
-                            label="Nombre de usuario"
-                            name="nickname"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su nombre de usuario!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-
-                        <Form.Item
-                            label="Nombre"
-                            name="name"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su nombre!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Apellido"
-                            name="lastname"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su apellido!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Dirección"
-                            name="address"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su direccion de residencia!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-
-                        <Form.Item
-                            label="País"
-                            name="country"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su país de residencia!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-
-                        <Form.Item
-                            label="Comunidad autónoma"
-                            name="state"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su comunidad autonoma de residencia!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-
-                        <Form.Item
-                            label="Ciudad"
-                            name="city"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su ciudad de residencia!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-
-
-                        <Form.Item
-                            label="Código postal"
-                            name="zip"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '¡Por favor, introduzca su codigo postal!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <br></br>
-                        <div style={{ display: "flex", justifyContent: "right", alignItems: "center" }}>
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit">
-                                    Guardar cambios
-                                </Button>
-                            </Form.Item>
-                        </div>
-                    </Form>
+                    {modaldata}
                 </Modal>
 
-
-            </div >
+                <div style={{ display: "flex", justifyContent: "right", alignItems: "center" }}>
+                    <Button type="primary" onClick={Venta}>
+                        vender
+                    </Button>
+                </div>
+            </div>
         </>
-    );
-});
+
+
+
+    )
+})
